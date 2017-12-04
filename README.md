016ec640268427d846ef689fe9f2e1c961574780
Vladimir Iakovlev <nvbn.rm@gmail.com>
2017-12-04 18:28:19 +0100

    #N/A: Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Castable KissCartoon
+
+Chrome extension that makes KissCartoon usable with Chromecast, but apparently makes
+it worse without Chromecast.
+
+The only feature is autoplay of the next episode in "fullscreen" on Chromecast.
+It can't be disabled and it's not a real fullscreen in the browser.
+
+You can leave "fullscreen" by pressing `Esc`.
+
+[![Available in the chrome web store](https://developer.chrome.com/webstore/images/ChromeWebStore_Badge_v2_206x58.png)](https://chrome.google.com/webstore/detail/bjcojegodnfapkdbbnmohcpkfemnjpf)
+
+## License MIT
---
+++ content.js
@@ -0,0 +1,41 @@
+const enableAutoplay = (player) => player.addEventListener('ended', () => {
+  window.localStorage.setItem('autoPlayingBefore', location.href);
+  document.getElementById('btnNext').click();
+}, false);
+
+const isContinuingPlaying = () => {
+  const previous = window.localStorage.getItem('autoPlayingBefore');
+  window.localStorage.removeItem('autoPlayingBefore');
+
+  const [previousBtn] = document.getElementsByClassName('preev');
+
+  return previousBtn.href === previous;
+}
+
+const enableFullscreen = (player) => {
+  const offlight = document.getElementById('offlight');
+  player.style.setProperty('position', 'fixed');
+  offlight.click();
+
+
+  let isExited = false;
+  document.addEventListener('keyup', ({keyCode}) => {
+    if (keyCode === 27 && !isExited) {
+      player.style.setProperty('position', '');
+      offlight.click();
+      isExited = true;
+    }
+  });
+};
+
+setTimeout(() => {
+  const player = document.getElementById('player_html5_html5_api');
+
+  if (player) {
+    enableAutoplay(player);
+    if (isContinuingPlaying()) {
+      enableFullscreen(player);
+    }
+  }
+}, 3000);
+r
